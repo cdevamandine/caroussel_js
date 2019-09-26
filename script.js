7282b66690d4b5f651f31e518730fc3d393b39e4
Amandine Carlier <dev-amandinecarlier@mailo.com>
2019-09-26 14:52:45 +0200

    objectives ticked

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// creation variable //
+const button = document.body.querySelector(`button`);
+const li = document.body.querySelector(`li`);
+
+// functions //
+button.addEventListener( `click`, () => alert(`ok `));
+
